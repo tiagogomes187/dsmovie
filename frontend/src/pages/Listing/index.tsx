629dddc6790582ae447a9678ce80e074826a0c3c
Tiago Gomes <tiagogomes187@gmail.com>
2022-01-14 22:11:53 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+  //FORMA ERRADA
+  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <Pagination />
@@ -27,7 +34,6 @@ function Listing() {
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-       
         </div>
       </div>
     </>
